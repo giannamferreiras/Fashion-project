c09940f20f24d4947469f5951c382c1234004727
gianna <giannamf45@gmail.com>
2023-06-26 10:14:09 -0400

    Initial Commit

---
+++ Fashion project/Fashion_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fashion_projectApp.swift
+//  Fashion project
+//
+//  Created by Gianna Ferreiras on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Fashion_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
